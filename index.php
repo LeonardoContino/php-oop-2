6632dbabe840eb7a228466e699e8476e6dd7a49b
LeonardoContino <leoconti2000@hotmail.it>
2023-02-20 21:44:01 +0100

    mod indirizzo img

--- index.php
+++ index.php
@@ -21,12 +21,12 @@ $dogs = new Categories('dogs');
 
 // prodotti
 
-$food_cat = new Food('crocchette','https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dm-drogeriemarkt.it%2Falmo-nature-crocchette-per-gatti-adulti-holistic-maintenance-con-pollo-e-tacchino-p8001154127539.html&psig=AOvVaw1tTCX6xNXxK05uTLrABxYy&ust=1677011337594000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKC6_Zr4pP0CFQAAAAAdAAAAABAF', $cats, 2, 'cibo');
-$food_dog = new Food('crocchette','https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.spesaonline24.it%2Fcane%2F1226-friskies-crocchette-per-cane-adulto-con-manzo-cereali-e-verdure-aggiunte-75kg&psig=AOvVaw0-8XLUS6Xb06l9xnq4hJBd&ust=1677011367675000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCKi1tan4pP0CFQAAAAAdAAAAABAF', $dogs, 2.50, 'cibo');
-$toy_dog = new Toy('osso','https://www.google.com/imgres?imgurl=https%3A%2F%2Fanimalmania.it%2Fvendita%2F3456-large_default%2Fchew-it-small-osso-curativo-cane.jpg&imgrefurl=https%3A%2F%2Fanimalmania.it%2Fvendita%2Figiene-orale-cane%2F1531-chew-it-small-osso-curativo-cane.html&tbnid=w3ITVoTHDz1a3M&vet=12ahUKEwi1-ZnQ-KT9AhVegaQKHXp1B8YQMygBegUIARCNAw..i&docid=U91T6_TmmLBtYM&w=458&h=458&q=osso%20came&ved=2ahUKEwi1-ZnQ-KT9AhVegaQKHXp1B8YQMygBegUIARCNAw', $dogs, 6, 'gioco');
-$toy_cat = new Toy('pallina','https://www.google.com/aclk?sa=l&ai=DChcSEwjop_Tf-KT9AhUJx3cKHQ-QBx4YABABGgJlZg&sig=AOD64_2t1tH__5QonmRcxNdGzNo3bFkgYQ&adurl&ctype=5&ved=2ahUKEwjXv-nf-KT9AhUSP-wKHTEHCqoQvhd6BAgBEFA', $cats, 2, 'gioco');
-$kennel_dog = new Kennel('casetta di legno','https://www.google.com/aclk?sa=l&ai=DChcSEwjop_Tf-KT9AhUJx3cKHQ-QBx4YABABGgJlZg&sig=AOD64_2t1tH__5QonmRcxNdGzNo3bFkgYQ&adurl&ctype=5&ved=2ahUKEwjXv-nf-KT9AhUSP-wKHTEHCqoQvhd6BAgBEFA', $dogs, 50, '3m x 3m');
-$kennel_cat = new Kennel('casetta per gatti','https://www.google.com/aclk?sa=l&ai=DChcSEwi3z9P6-KT9AhXo_3cKHVqNAe4YABABGgJlZg&sig=AOD64_1ZzDEDWGq7d1UQxYvS6uN__9jh0Q&adurl&ctype=5&ved=2ahUKEwj47Mf6-KT9AhVDwwIHHbJpCDsQvhd6BAgBEFg', $cats, 30, '3m x 3m');
+$food_cat = new Food('crocchette','https://shop-cdn-m.mediazs.com/bilder/x/kg/almo/nature/holistic/crocchette/cane/7/400/icon_percent_template_1000x1000_int_1__7.jpg', $cats, 2, 'cibo');
+$food_dog = new Food('crocchette','https://shop-cdn-m.mediazs.com/bilder/royal/canin/digestive/care/crocchette/per/gatti/6/400/icon_topseller_1_2021_12_28t145443_494_6.jpg', $dogs, 2.50, 'cibo');
+$toy_dog = new Toy('osso','https://shop-cdn-m.mediazs.com/bilder/nylabone/osso/annodato/al/gusto/di/manzo/per/cani/1/400/225398_pla_nylabone_rohhautknochen_rindfleischgeschmack_hs_04_1.jpg', $dogs, 6, 'gioco');
+$toy_cat = new Toy('pallina','https://shop-cdn-m.mediazs.com/bilder/palline/in/lana/cosma/1/400/85434_pla_cosma_wollbaelle_fg_1671_1.jpg', $cats, 2, 'gioco');
+$kennel_dog = new Kennel('casetta di legno','https://m.media-amazon.com/images/I/71nf1O2IBML._AC_SX679_.jpg', $dogs, 50, '3m x 3m');
+$kennel_cat = new Kennel('casetta per gatti','https://m.media-amazon.com/images/I/810NQPqAv5L._AC_SY300_SX300_.jpg', $cats, 30, '3m x 3m');
 
 $products =[$food_cat, $food_dog, $toy_cat, $toy_dog, $kennel_cat, $kennel_dog];
 
@@ -54,7 +54,7 @@ $products =[$food_cat, $food_dog, $toy_cat, $toy_dog, $kennel_cat, $kennel_dog];
            <?php foreach($products as $product) :?> 
             <div class="col-4 p-3">
                 <div class="card p-3">
-                <img src="<?=$product->image ?>" class="card-img-top" alt="<?=$product->name?>">
+                <img src="<?=$product->image?>" class="card-img-top" alt="<?=$product->name?>">
                     <div class="card-body">
                     <h5 class="card-title">Nome Prodotto: <?= $product->name ?></h5>
                     <p class="card-text">Categoria: <?= $product->categories->name ?></p>
